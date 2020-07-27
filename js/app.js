0207074cf697bdc6048f0b4d75a597345dfbbc8f
Francesco Bellini <francescobellini92@gmail.com>
2020-07-28 00:23:39 +0200

    adding scroll-to-top button, implemeting hide-on-scroll logic

--- js/app.js
+++ js/app.js
@@ -1,31 +1,52 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
+// ################ //
 
-const navbarList = document.querySelector('#navbar__list');
-const sections = document.querySelectorAll('section');
+const navbarList = $('#navbar__list');
+const sections = $('section');
+const scrollTopButton = $('#scroll-top-button');
 let anchors;
+let updateUIOnScrollTimeout;
+// ################ //
+
+function $(selector) {
+  if (selector.includes('#')) {
+    return document.querySelector(selector);
+  }
+  return document.querySelectorAll(selector);
+}
+
+function hasClass(element, className) {
+  return element.className.includes(className);
+}
+
+function getHeigthFromTopViewport(element) {
+  return element.getBoundingClientRect().y;
+}
 
 function applyActiveClass(targetSection) {
-  if (!targetSection.className.includes('your-active-class')) {
-    sections.forEach((section) => section.classList.remove('your-active-class'));
+  if (!hasClass(targetSection, 'your-active-class')) {
+    sections.forEach((section) =>
+      section.classList.remove('your-active-class')
+    );
     targetSection.classList.add('your-active-class');
-    const targetAnchor = document.querySelector(`a[href="#${targetSection.id}"]`);
+    const targetAnchor = $(`a[href="#${targetSection.id}"]`);
     anchors.forEach((section) => section.classList.remove('your-active-class'));
     targetAnchor.classList.add('your-active-class');
   }
 }
+
+function hide(element) {
+  element.classList.add('hidden');
+}
+
+function show(element) {
+  element.classList.remove('hidden');
+}
+
+function scrolledBelowPageFold() {
+  return visualViewport.pageTop >= visualViewport.height;
+}
+
+// ################ //
 
 function populateNavbarList() {
   const domFragment = document.createDocumentFragment();
@@ -44,36 +65,58 @@ function populateNavbarList() {
   }
 }
 
-// Add class 'active' to section when near top of viewport
 function addOnScrollHandler() {
-  window.addEventListener('scroll', function () {
+  window.addEventListener('scroll', () => {
+    clearInterval(updateUIOnScrollTimeout);
+    hide(navbarList);
+    hide(scrollTopButton);
     sections.forEach((section) => {
-      // y is the offset between element y coordinates and viewport top
-      const sectionHeigth = section.getBoundingClientRect().y;
+      const sectionHeigth = getHeigthFromTopViewport(section);
       if (sectionHeigth <= 100 && sectionHeigth >= -100) {
         applyActiveClass(section);
       }
     });
+    updateUIOnScrollTimeout = setTimeout(() => {
+      show(navbarList);
+      if (scrolledBelowPageFold()) {
+        show(scrollTopButton);
+      }
+    }, 100);
   });
 }
 
 function addOnClickHandler() {
-  navbarList.addEventListener('click', function (event) {
+  // using event delegation to avoid attaching too many handlers
+  navbarList.addEventListener('click', event => {
     event.preventDefault();
     const anchorRef = event.target.attributes.href.value;
-    const targetSection = document.querySelector(anchorRef);
-    const yCoord = targetSection.getBoundingClientRect().top;
+    const targetSection = $(anchorRef);
+    const yCoord = getHeigthFromTopViewport(targetSection);
     window.scrollTo({
       top: window.pageYOffset + yCoord,
       behavior: 'smooth'
     });
   });
+
+  scrollTopButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  sections.forEach((section) => {
+    section.addEventListener('click', () => {
+      section.classList.toggle('collapsed');
+    });
+  });
 }
 
-// Scroll to anchor ID using scrollTO event
-window.addEventListener('load', function () {
+// ################ //
+
+window.addEventListener('load', () => {
   populateNavbarList();
-  anchors = document.querySelectorAll('a');
+  anchors = $('a');
   addOnScrollHandler();
   addOnClickHandler();
 });
