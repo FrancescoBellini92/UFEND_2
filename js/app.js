16b2bb2bb631bdd9815931dbfcfaa1dd0c0f5421
Francesco Bellini <francescobellini92@gmail.com>
2020-07-28 00:55:00 +0200

    refactor code to avoid global vars - main closure added

--- js/app.js
+++ js/app.js
@@ -1,122 +1,129 @@
-// ################ //
+function main() {
+  // ################ //
 
-const navbarList = $('#navbar__list');
-const sections = $('section');
-const scrollTopButton = $('#scroll-top-button');
-let anchors;
-let updateUIOnScrollTimeout;
-// ################ //
+  const navbarList = $('#navbar__list');
+  const sections = $('section');
+  const scrollTopButton = $('#scroll-top-button');
+  let anchors;
+  let updateUIOnScrollTimeout;
+  // ################ //
 
-function $(selector) {
-  if (selector.includes('#')) {
-    return document.querySelector(selector);
+  function $(selector) {
+    if (selector.includes('#')) {
+      return document.querySelector(selector);
+    }
+    return document.querySelectorAll(selector);
   }
-  return document.querySelectorAll(selector);
-}
 
-function hasClass(element, className) {
-  return element.className.includes(className);
-}
-
-function getHeigthFromTopViewport(element) {
-  return element.getBoundingClientRect().y;
-}
-
-function applyActiveClass(targetSection) {
-  if (!hasClass(targetSection, 'your-active-class')) {
-    sections.forEach((section) =>
-      section.classList.remove('your-active-class')
-    );
-    targetSection.classList.add('your-active-class');
-    const targetAnchor = $(`a[href="#${targetSection.id}"]`);
-    anchors.forEach((section) => section.classList.remove('your-active-class'));
-    targetAnchor.classList.add('your-active-class');
+  function hasClass(element, className) {
+    return element.className.includes(className);
   }
-}
 
-function hide(element) {
-  element.classList.add('hidden');
-}
-
-function show(element) {
-  element.classList.remove('hidden');
-}
-
-function scrolledBelowPageFold() {
-  return visualViewport.pageTop >= visualViewport.height;
-}
-
-// ################ //
-
-function populateNavbarList() {
-  const domFragment = document.createDocumentFragment();
-  sections.forEach((section) => populateFragment(section));
-  navbarList.appendChild(domFragment);
-
-  function populateFragment(section) {
-    const listChild = document.createElement('li');
-
-    const anchorText = section.attributes['data-nav'].textContent;
-    const anchorRef = section.id;
-
-    listChild.innerHTML = `<a href="#${anchorRef}">${anchorText}</a>`;
-
-    domFragment.appendChild(listChild);
+  function getHeigthFromTopViewport(element) {
+    return element.getBoundingClientRect().y;
   }
-}
 
-function addOnScrollHandler() {
-  window.addEventListener('scroll', () => {
-    clearInterval(updateUIOnScrollTimeout);
-    hide(navbarList);
-    hide(scrollTopButton);
+  function applyActiveClass(targetSection) {
+    if (!hasClass(targetSection, 'your-active-class')) {
+      sections.forEach((section) =>
+        section.classList.remove('your-active-class')
+      );
+      targetSection.classList.add('your-active-class');
+      const targetAnchor = $(`a[href="#${targetSection.id}"]`);
+      anchors.forEach((section) =>
+        section.classList.remove('your-active-class')
+      );
+      targetAnchor.classList.add('your-active-class');
+    }
+  }
+
+  function hide(element) {
+    element.classList.add('hidden');
+  }
+
+  function show(element) {
+    element.classList.remove('hidden');
+  }
+
+  function scrolledBelowPageFold() {
+    return visualViewport.pageTop >= visualViewport.height;
+  }
+
+  // ################ //
+
+  function populateNavbarList() {
+    const domFragment = document.createDocumentFragment();
+    sections.forEach((section) => populateFragment(section));
+    navbarList.appendChild(domFragment);
+
+    function populateFragment(section) {
+      const listChild = document.createElement('li');
+
+      const anchorText = section.attributes['data-nav'].textContent;
+      const anchorRef = section.id;
+
+      listChild.innerHTML = `<a href="#${anchorRef}">${anchorText}</a>`;
+
+      domFragment.appendChild(listChild);
+    }
+  }
+
+  function addOnScrollHandler() {
+    window.addEventListener('scroll', () => {
+      clearInterval(updateUIOnScrollTimeout);
+      hide(navbarList);
+      hide(scrollTopButton);
+      sections.forEach((section) => {
+        const sectionHeigth = getHeigthFromTopViewport(section);
+        if (sectionHeigth <= 100 && sectionHeigth >= -100) {
+          applyActiveClass(section);
+        }
+      });
+      updateUIOnScrollTimeout = setTimeout(() => {
+        show(navbarList);
+        if (scrolledBelowPageFold()) {
+          show(scrollTopButton);
+        }
+      }, 100);
+    });
+  }
+
+  function addOnClickHandler() {
+    // using event delegation to avoid attaching too many handlers
+    navbarList.addEventListener('click', (event) => {
+      event.preventDefault();
+      const anchorRef = event.target.attributes.href.value;
+      const targetSection = $(anchorRef);
+      const yCoord = getHeigthFromTopViewport(targetSection);
+      window.scrollTo({
+        top: window.pageYOffset + yCoord,
+        behavior: 'smooth'
+      });
+    });
+
+    scrollTopButton.addEventListener('click', () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+
     sections.forEach((section) => {
-      const sectionHeigth = getHeigthFromTopViewport(section);
-      if (sectionHeigth <= 100 && sectionHeigth >= -100) {
-        applyActiveClass(section);
-      }
+      section.addEventListener('click', () => {
+        section.classList.toggle('collapsed');
+      });
     });
-    updateUIOnScrollTimeout = setTimeout(() => {
-      show(navbarList);
-      if (scrolledBelowPageFold()) {
-        show(scrollTopButton);
-      }
-    }, 100);
-  });
-}
-
-function addOnClickHandler() {
-  // using event delegation to avoid attaching too many handlers
-  navbarList.addEventListener('click', event => {
-    event.preventDefault();
-    const anchorRef = event.target.attributes.href.value;
-    const targetSection = $(anchorRef);
-    const yCoord = getHeigthFromTopViewport(targetSection);
-    window.scrollTo({
-      top: window.pageYOffset + yCoord,
-      behavior: 'smooth'
-    });
-  });
-
-  scrollTopButton.addEventListener('click', () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  });
-
-  sections.forEach((section) => {
-    section.addEventListener('click', () => {
-      section.classList.toggle('collapsed');
-    });
-  });
+  }
+  return () => {
+    populateNavbarList();
+    anchors = $('a');
+    addOnScrollHandler();
+    addOnClickHandler();
+  }
 }
 
 // ################ //
 
 window.addEventListener('load', () => {
-  populateNavbarList();
-  anchors = $('a');
-  addOnScrollHandler();
-  addOnClickHandler();
+  main()();
 });
